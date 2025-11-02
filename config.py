45d8833c62d88351f97f964ee9880179ce0513f0
fred96-afk <i2420085@continental.edu.pe>
2025-11-02 17:07:25 -0500

    deploy

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mssql+pymssql://sa:edgardo.,01@localhost:1433/test_api'
+        'mssql+pymssql://Freddy_SQLLogin_1:52vgexnsdj@test_api.mssql.somee.com/test_api?TrustServerCertificate=True'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your-jwt-secret-key'
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance/uploads')
