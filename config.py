9d006722b95985e92c2002df57360b472f7e42f9
fred96-afk <i2420085@continental.edu.pe>
2025-11-02 22:02:29 -0500

    enviar al profesor

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mssql+pyodbc://Freddy_SQLLogin_1:52vgexnsdj@test_api.mssql.somee.com/test_api?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes&TrustServerCertificate=yes'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mssql+pymssql://sa:979052Fr.@DESKTOP-H9LM8AG\\DEVELOPMENT/test_api'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your-jwt-secret-key'
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance/uploads')
