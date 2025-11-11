630afc30649630ba4c0f1c01b90fd1ed31980f29
EdgardoJorge <i2310402@continental.edu.pe>
2025-11-11 10:57:52 -0500

    cloudinary

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mssql+pymssql://sa:992705Fr@user/test_api'
+    # mssql+pyodbc://user:password@host\instance_name/database?driver=...
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://Freddy_SQLLogin_1:52vgexnsdj@test_api.mssql.somee.com/test_api?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=True'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your-jwt-secret-key'
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance/uploads')
-    CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME') or 'your_cloud_name'
-    CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY') or 'your_api_key'
-    CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET') or 'your_api_secret'
-
+    CLOUDINARY_CLOUD_NAME = 'dcvnwpwnu'
+    CLOUDINARY_API_KEY = '312535877423432'
+    CLOUDINARY_API_SECRET = '3P1aZsINAFGBnNsIoyH2zJDreRo'
--- main.py
+++ main.py
@@ -2,11 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+import cloudinary
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Configure Cloudinary
+cloudinary.config(
+    cloud_name=app.config['CLOUDINARY_CLOUD_NAME'],
+    api_key=app.config['CLOUDINARY_API_KEY'],
+    api_secret=app.config['CLOUDINARY_API_SECRET']
+)
+
 CORS(app, resources={r"/*": {"origins": ["https://frontend-angular-fxx8.vercel.app", "http://localhost:4200"]}})
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
