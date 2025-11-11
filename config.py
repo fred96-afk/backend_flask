de5c60cb1a75e3b6a3908b96cfa3fea04d5ea00d
EdgardoJorge <i2310402@continental.edu.pe>
2025-11-11 13:21:34 -0500

    cloudinary_driver_update

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # mssql+pyodbc://user:password@host\instance_name/database?driver=...
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://Freddy_SQLLogin_1:52vgexnsdj@test_api.mssql.somee.com/test_api?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=True'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://Freddy_SQLLogin_1:52vgexnsdj@test_api.mssql.somee.com/test_api?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=True'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your-jwt-secret-key'
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance/uploads')
